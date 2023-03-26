8107c556cff6fac4ce422ad9566ca87a5d099c66
Rafael Ribeiro <rafael.ribeiro.rr11@gmail.com>
2023-03-26 17:02:08 +0100

    Last changes

--- src/myCloudServer.java
+++ src/myCloudServer.java
@@ -7,6 +7,11 @@ import thread.ServerThread;
 
 public class myCloudServer {
 
+	/**
+	 * Check if port number is valid
+	 * @String[] list of arguments to check 
+	 * @return Port in integer
+	 */
 	private static int verifyPort(String[] args) {
 		
 		String port = "";
@@ -19,12 +24,20 @@ public class myCloudServer {
 		return Integer.parseInt(port);
 	}
 	
+	/**
+	 * Initialize server
+	 * @String[] list of arguments to check 
+	 */
 	public static void main(String[] args) throws IOException  {
 		int port = verifyPort(args);
 		myCloudServer server = new myCloudServer();
 		server.startServer(port);
 	}
 	
+	/**
+	 * Method to connect the server
+	 * @integer port number
+	 */
 	private void startServer(int port) throws IOException {
 		ServerSocket sSoc = null;
 		sSoc = new ServerSocket(port);
